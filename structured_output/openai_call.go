b559ea45758c7668cb330146a40f71eeeaf02379
Thomás Freire da Silva Reis <thomas.fsr@gmail.com>
2025-09-17 23:20:51 -0300

    working

--- structured_output/openai_call.go
+++ structured_output/openai_call.go
@@ -13,16 +13,25 @@ import (
 )
 
 // A struct that will be converted to a Structured Outputs response schema
-type HistoricalComputer struct {
-	Origin       Origin   `json:"origin" jsonschema_description:"The origin of the computer"`
-	Name         string   `json:"full_name" jsonschema_description:"The name of the device model"`
-	Legacy       string   `json:"legacy" jsonschema:"enum=positive,enum=neutral,enum=negative" jsonschema_description:"Its influence on the field of computing"`
-	NotableFacts []string `json:"notable_facts" jsonschema_description:"A few key facts about the computer"`
+// type HistoricalComputer struct {
+// 	Origin       Origin   `json:"origin" jsonschema_description:"The origin of the computer"`
+// 	Name         string   `json:"full_name" jsonschema_description:"The name of the device model"`
+// 	Legacy       string   `json:"legacy" jsonschema:"enum=positive,enum=neutral,enum=negative" jsonschema_description:"Its influence on the field of computing"`
+// 	NotableFacts []string `json:"notable_facts" jsonschema_description:"A few key facts about the computer"`
+// }
+
+// type Origin struct {
+// 	YearBuilt    int64  `json:"year_of_construction" jsonschema_description:"The year it was made"`
+// 	Organization string `json:"organization" jsonschema_description:"The organization that was in charge of its development"`
+// }
+
+type extractTask struct {
+	LabelTask       string `json:"label_task" jsonschema:"enum=query_data,enum=update,enum=chat" jsonschema_description:"The label of the task if it is quering database, updating the database or neither just chatting."`
+	TaskDescription string `json:"task_description" jsonschema_description:"The task with the main informations to execute the user request"`
 }
 
-type Origin struct {
-	YearBuilt    int64  `json:"year_of_construction" jsonschema_description:"The year it was made"`
-	Organization string `json:"organization" jsonschema_description:"The organization that was in charge of its development"`
+type ListOfTasks struct {
+	Tasks []extractTask `json:"tasks" jsonschema_description:"List of tasks extracted from the user input."`
 }
 
 func GenerateSchema[T any]() interface{} {
@@ -38,7 +47,7 @@ func GenerateSchema[T any]() interface{} {
 }
 
 // Generate the JSON schema at initialization time
-var HistoricalComputerResponseSchema = GenerateSchema[HistoricalComputer]()
+var ListOfTasksSchema = GenerateSchema[ListOfTasks]()
 
 func main() {
 	_ = godotenv.Load()
@@ -50,15 +59,15 @@ func main() {
 	)
 	ctx := context.Background()
 
-	question := "What computer ran the first neural network?"
+	question := "Add 5 kilograms of rice to my data and retrieves how many eggs are there"
 
 	print("> ")
 	println(question)
 
 	schemaParam := openai.ResponseFormatJSONSchemaJSONSchemaParam{
-		Name:        "historical_computer",
-		Description: openai.String("Notable information about a computer"),
-		Schema:      HistoricalComputerResponseSchema,
+		Name:        "extract_tasks",
+		Description: openai.String("Tasks extracted from users input"),
+		Schema:      ListOfTasksSchema,
 		Strict:      openai.Bool(true),
 	}
 
@@ -79,19 +88,18 @@ func main() {
 	}
 
 	// The model responds with a JSON string, so parse it into a struct
-	var historicalComputer HistoricalComputer
-	err = json.Unmarshal([]byte(chat.Choices[0].Message.Content), &historicalComputer)
+	var listoftasks ListOfTasks
+	err = json.Unmarshal([]byte(chat.Choices[0].Message.Content), &listoftasks)
 	if err != nil {
 		panic(err.Error())
 	}
 
 	// Use the model's structured response with a native Go struct
-	fmt.Printf("Name: %v\n", historicalComputer.Name)
-	fmt.Printf("Year: %v\n", historicalComputer.Origin.YearBuilt)
-	fmt.Printf("Org: %v\n", historicalComputer.Origin.Organization)
-	fmt.Printf("Legacy: %v\n", historicalComputer.Legacy)
-	fmt.Printf("Facts:\n")
-	for i, fact := range historicalComputer.NotableFacts {
-		fmt.Printf("%v. %v\n", i+1, fact)
+	fmt.Printf("tasks: %v\n", listoftasks.Tasks)
+
+	for i, task := range listoftasks.Tasks {
+		fmt.Printf("%v. %v\n", i+1, task.LabelTask)
+		fmt.Printf("%v. %v\n", i+1, task.TaskDescription)
+
 	}
 }
