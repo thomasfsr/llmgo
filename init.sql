59beb8a8da7c544b88026742411e51e3da487536
Thomás Freire da Silva Reis <thomas.fsr@gmail.com>
2025-10-28 23:33:29 -0300

    returning to llm

--- init.sql
+++ init.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  first_name CHAR(20),
-  last_name CHAR(50),
-  whatsapp BIGINT,
-  active BOOLEAN
+CREATE TABLE IF NOT EXISTS users ( 
+  id SERIAL PRIMARY KEY, 
+  first_name CHAR(20), 
+  last_name CHAR(50), 
+  whatsapp BIGINT, 
+  active BOOLEAN, 
   created_at TIMESTAMP,
 )
 
@@ -13,3 +13,11 @@ CREATE TABLE IF NOT EXISTS messages (
   message CHAR(100),
   created_at TIMESTAMP,
 )
+
+CREATE TABLE IF NOT EXISTS exercise_sets (
+  id SERIAL PRIMARY KEY,
+  exercise CHAR(100),
+  weight INT,
+  reps INT,
+  created_at TIMESTAMP,
+) 
